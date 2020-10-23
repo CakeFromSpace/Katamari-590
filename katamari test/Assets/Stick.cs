8168e5398050cfd341f2b1c3b144680808345114
CakeFromSpace <yunjoey@rocketmail.com>
2020-10-23 14:33:12 -0400

    Add katamari test game

---
+++ katamari test/Assets/Stick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stick : MonoBehaviour
+{
+    public float size;
+    Rigidbody rd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "sticky")
+        {
+            transform.parent = collision.transform;
+            gameObject.tag = "sticky";
+            Destroy(rd);
+        }
+        
+    }
+}
