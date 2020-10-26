f975d4a802c57dcac3caf2080d7880e84ddca627
CakeFromSpace <yunjoey@rocketmail.com>
2020-10-25 21:36:02 -0400

    Made better pickup mechanics

--- katamari test/Assets/Stick.cs
+++ katamari test/Assets/Stick.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using UnityEngine;
 
+//Author Joseph Yunis
 public class Stick : MonoBehaviour
 {
     public float size;
     Rigidbody rd;
+    Collider coll;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         rd = gameObject.GetComponent<Rigidbody>();
+        coll = gameObject.GetComponent<Collider>();
     }
 
     // Update is called once per frame
@@ -20,12 +25,25 @@ public class Stick : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "sticky")
+        if(collision.collider.gameObject.tag == "sticky" || collision.collider.gameObject.tag == "pickup")
         {
-            transform.parent = collision.transform;
-            gameObject.tag = "sticky";
+            Debug.Log(name + collision.collider.gameObject.name);
+
+            Vector3 loc = Vector3.Normalize(player.transform.position - transform.position)*player.GetComponent<SphereCollider>().radius;
+
+            Debug.Log(loc);
+            
+            
+            transform.position = transform.TransformPoint(loc);
+            transform.parent = player.transform;
+            //gameObject.tag = "sticky";
+            SphereCollider s = player.gameObject.GetComponent<SphereCollider>();
+            s.radius += size;
             Destroy(rd);
         }
-        
+        else if(collision.collider.gameObject.tag == "pickup")
+        {
+            Physics.IgnoreCollision(coll, collision.collider);
+        }
     }
 }
