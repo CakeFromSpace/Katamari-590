62325fe7de44ada5d79aaab23131e033e0b28bc9
CakeFromSpace <yunjoey@rocketmail.com>
2020-10-27 16:30:57 -0400

    Really put everything in root

---
+++ Assets/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    public float turnspeed;
+    public Vector3 forward;
+    float theta;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        theta = 0;
+        forward = new Vector3(Mathf.Sin(theta), 0, Mathf.Cos(theta));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(forward*speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(-forward * speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            theta -= turnspeed/100f;
+            
+            forward = new Vector3(Mathf.Sin(theta), 0, Mathf.Cos(theta));
+            forward.Normalize();
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            theta += turnspeed/100f;
+
+            forward = new Vector3(Mathf.Sin(theta), 0, Mathf.Cos(theta));
+            forward.Normalize();
+        }
+    }
+}
