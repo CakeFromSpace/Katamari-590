d05d0083e5a7ab7628aa94f9bd7baa0dc9e4f593
CakeFromSpace <yunjoey@rocketmail.com>
2020-10-27 20:34:35 -0400

    Made a separate branch to continue working on player controller

--- Assets/Stick.cs
+++ Assets/Stick.cs
@@ -9,7 +9,8 @@ public class Stick : MonoBehaviour
     public float size;
     Rigidbody rd;
     Collider coll;
-    public GameObject player;
+    public GameObject katamari;
+    public GameObject constellation;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,25 +26,39 @@ public class Stick : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.collider.gameObject.tag == "sticky" || collision.collider.gameObject.tag == "pickup")
+        if(collision.collider.gameObject.tag== "sticky")
         {
-            Debug.Log(name + collision.collider.gameObject.name);
-
-            Vector3 loc = Vector3.Normalize(player.transform.position - transform.position)*player.GetComponent<SphereCollider>().radius;
-
-            Debug.Log(loc);
-            
-            
-            transform.position = transform.TransformPoint(loc);
-            transform.parent = player.transform;
-            //gameObject.tag = "sticky";
-            SphereCollider s = player.gameObject.GetComponent<SphereCollider>();
-            s.radius += size;
+            Vector3 loc = Vector3.Normalize(katamari.transform.position - transform.position) * katamari.GetComponent<SphereCollider>().radius*-1;
+            Debug.Log(loc.magnitude);
+            transform.position = katamari.transform.position + loc;//  transform.TransformPoint(loc);
+            transform.parent = constellation.transform;
+            gameObject.tag = "sticky";
+            SphereCollider s = katamari.gameObject.GetComponent<SphereCollider>();
+            s.radius += size*2;
             Destroy(rd);
         }
-        else if(collision.collider.gameObject.tag == "pickup")
-        {
-            Physics.IgnoreCollision(coll, collision.collider);
-        }
     }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //   if(collision.collider.gameObject.tag == "sticky" || (collision.collider.gameObject.tag == "pickup" &&collision.collider.gameObject.transform.parent))
+    //    {
+    //        Debug.Log(name + collision.collider.gameObject.name);
+    //
+    //        Vector3 loc = Vector3.Normalize(player.transform.position - transform.position)*player.GetComponent<SphereCollider>().radius;
+    //
+    //        Debug.Log(loc);
+    //        
+    //        
+    //        transform.position = transform.TransformPoint(loc);
+    //        transform.parent = player.transform;
+    //gameObject.tag = "sticky";
+    //        SphereCollider s = player.gameObject.GetComponent<SphereCollider>();
+    //        s.radius += size;
+    //        Destroy(rd);
+    //    }
+    //    if(collision.collider.gameObject.tag == "pickup")
+    //    {
+    //        Physics.IgnoreCollision(coll, collision.collider);
+    //    }
+    //}
 }
