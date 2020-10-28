992e9b0d93212d9162cfeef3b6bd4ef8239fddd7
Judge Russell <judgerussell@umass.edu>
2020-10-28 16:50:24 -0400

    trying to push this again...

---
+++ Assets/Scripts/Chase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+    public GameObject target;
+    public float height;
+    public float trailingdistance;
+    public float pandown;
+    Movement targetinfo;
+    void Start()
+    {
+        targetinfo = target.GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SphereCollider s = target.gameObject.GetComponent<SphereCollider>();
+        trailingdistance = 6 * target.transform.localScale.x * s.radius;
+        height = 2 * target.transform.localScale.x * s.radius;
+        transform.rotation = Quaternion.LookRotation(targetinfo.forward);
+        transform.Rotate(new Vector3(pandown,0,0));
+
+        Vector3 targetpos = target.transform.position;
+        targetpos -= trailingdistance * targetinfo.forward;
+        targetpos.y += height;
+        transform.position = targetpos;
+    }
+}
---
+++ Assets/Scripts/PlayerStick.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using UnityEngine;
+
+//Author Joseph Yunis
+public class PlayerStick : MonoBehaviour
+{
+    Rigidbody rd;
+    Collider coll;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.collider.gameObject;
+
+        if(collision.collider.gameObject.tag == "pickup")
+        {
+
+            Debug.Log(name + other.name);
+
+            Vector3 loc = Vector3.Normalize(transform.position - other.transform.position)*this.gameObject.GetComponent<SphereCollider>().radius;
+
+            Debug.Log(loc);
+            
+            SphereCollider s = this.gameObject.GetComponent<SphereCollider>();
+            MeshCollider m = other.gameObject.GetComponent<MeshCollider>();
+            if(Vector3.Magnitude(s.bounds.size) > Vector3.Magnitude(m.bounds.size))
+            {
+                other.transform.position = transform.TransformPoint(loc);
+                other.transform.parent = transform;
+                other.tag = "sticky";
+                m.convex = true;
+            }
+
+            s.radius += Vector3.Magnitude(m.bounds.size) / 1000;
+
+        }
+    }
+}
