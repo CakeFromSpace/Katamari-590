b5081b8138544bd0ef263ffbae68984875cb9c7c
Debankita15 <debankitabas@umass.edu>
2020-12-01 18:12:47 -0500

    ui

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+  
+    public void HowtoPlay()
+    {
+        SceneManager.LoadScene("HowtoPlay");
+
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    
+}
